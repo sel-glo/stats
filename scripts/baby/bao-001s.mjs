449cd9c833b762dccf9e167d2b7ee50180908341
sel-glo <selglo2001@gmail.com>
2025-08-10 00:59:36 +0300

    Update bao-001s.mjs

--- scripts/baby/bao-001s.mjs
+++ scripts/baby/bao-001s.mjs
@@ -10,9 +10,9 @@ const INPUT_PATH = path.join(__dirname, '..', '..', 'daily', 'baby', 'babyother'
 const OUTPUT_DIR = path.join(__dirname, '..', '..', 'daily', 'baby', 'babyother', 'sliced');
 
 // ===== تنظیمات شما (بر حسب CSS px) =====
-const VIEWPORT_WIDTH_CSS = 90;     // عرض     
-const startY_css = 2;              // فاصله از بالا (CSS px)
-const trimRight_css = 60;           // مقدار حاشیه‌ای که از چپ حذف می‌شود (CSS px)
+const VIEWPORT_WIDTH_CSS = 120;     // عرض     
+const startY_css = 1;              // فاصله از بالا (CSS px)
+const trimRight_css = 70;           // مقدار حاشیه‌ای که از چپ حذف می‌شود (CSS px)
 const itemCount = 100;              // تعداد آیتم‌های برش )
 
 // =======================================
